cb9736c86edb6e331885d22135c5a1280d87a5f3
javeed-Ali-001 <javeedali806@gmail.com>
2023-07-27 11:55:01 +0000

    this is cls react-context-p3

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+
+import {Component} from 'react'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+import CartContext from './context/CartContext'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  addCartItem = product => {
+    const {cartList} = this.state
+    console.log('prod', product)
+    this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    console.log('cartList', cartList)
+  }
+
+  deleteCartItem = () => {}
+
+  render() {
+    const {cartList} = this.state
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          deleteCartItem: this.deleteCartItem,
+        }}
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/login" component={LoginForm} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute exact path="/products" component={Products} />
+            <ProtectedRoute
+              exact
+              path="/products/:id"
+              component={ProductItemDetails}
+            />
+            <ProtectedRoute exact path="/cart" component={Cart} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="not-found" />
+          </Switch>
+        </BrowserRouter>
+      </CartContext.Provider>
+    )
+  }
+}
+
+export default App
